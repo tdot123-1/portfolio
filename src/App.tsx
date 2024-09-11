fce75bb93b22af09de263965ecc8b103ed7a612e
Thomas Kruithof <tkruithof95@gmail.com>
2024-09-11 19:52:31 +0200

    init shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <Button>New Button</Button>
+      </div>
     </>
   );
 }
