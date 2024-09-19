fddd67062133edc6380222241f31c11eed01103b
Thomas Kruithof <tkruithof95@gmail.com>
2024-09-19 11:37:43 +0200

    add toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>
 );
--- src/pages/SkillsPage.tsx
+++ src/pages/SkillsPage.tsx
@@ -1,8 +1,44 @@
 import { skillsData } from "@/data/skillsData";
 import { columns } from "@/components/custom/table/columns";
 import DataTable from "@/components/custom/table/DataTable";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { IconBulb, IconDeviceMobile } from "@tabler/icons-react";
 
 const SkillsPage = () => {
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 440);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth > 440);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (!isWideScreen) {
+      toast(
+        <div className=" flex flex-col">
+          <div className="flex justify-start items-center gap-1.5 text-slate-600">
+            <IconBulb />
+            <h3 className="font-bold">Tip!</h3>
+          </div>
+          <div>
+            <div className="flex justify-start items-center gap-1.5 text-slate-600">
+              <span className="text-xs">
+                Rotate your device sideways to display full screen
+              </span>
+              <IconDeviceMobile className="custom-rotate-device" />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }, [isWideScreen]);
+
   return (
     <>
       <div className="bg-slate-100 min-h-screen pb-8">
